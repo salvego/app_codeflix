3e8fce0bd041d016facc6a241bef928e424aed6e
salvego <fael_ssantos@yahoo.com.br>
2024-11-21 00:05:12 -0300

    subindo a versao final

--- src/app/watch/[id]/page.tsx
+++ src/app/watch/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getMovieById } from '../../service/MovieService';
 import Player from '../../components/Player';
+import Cabecalho from '../../components/Cabecalho';
 
 interface IWatchProps {
   params: Promise<{
@@ -14,7 +15,7 @@ export default async function Watch({ params }: IWatchProps) {
   if (!movie) {
     return (
       <div className='flex h-screen justify-center align-middle'>
-        {/* <Header /> */}
+        <Cabecalho />
         <main className='flex flex-1 flex-col items-center justify-center px-20 text-center'>
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             Sorry, this movie is not available.
