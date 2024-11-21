6bd76f079f771fc441155b5da8ded0b48a3850fe
salvego <fael_ssantos@yahoo.com.br>
2024-11-20 23:30:35 -0300

    teste

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const path = require('path');
+
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
   images: {
     domains: ["m.media-amazon.com"],
   }
